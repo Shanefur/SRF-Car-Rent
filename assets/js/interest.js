28e985c42550e8ced77189f5ac24f3769a9ac5e6
Shanefur <shanerayfurtado@gmail.com>
2023-11-15 22:36:35 +0530

    formula for interest

--- assets/js/interest.js
+++ assets/js/interest.js
@@ -8,54 +8,51 @@ function calculateLoan() {
   const category = document.getElementById('category').value;
 
   if (isNaN(loanAmount) || isNaN(downPayment) || isNaN(loanTerm)) {
-    alert('Please enter valid numerical values.');
-    return;
+      alert('Please enter valid numerical values.');
+      return;
   }
 
   // Enforce a minimum loan amount of 10000 Rs
   if (loanAmount < 10000) {
-    alert('The minimum loan amount is 10000 Rs.');
-    return;
+      alert('The minimum loan amount is 10000 Rs.');
+      return;
   }
 
   // Assuming three payment tiers with different configurations
   const paymentTiers = {
-    Junior: { interestRate: 0.05, downPaymentPercentage: 0.2, termMultiplier: 1 },
-    Senior: { interestRate: 0.1, downPaymentPercentage: 0.1, termMultiplier: 0.6 },
-    SeniorCitizen: { interestRate: 0.08, downPaymentPercentage: 0.15, termMultiplier: 0.8 },
+      Junior: { interestRate: 0.05 },
+      Senior: { interestRate: 0.1 },
+      SeniorCitizen: { interestRate: 0.08 },
   };
 
   // Get the selected payment tier based on the category
   const selectedTier = paymentTiers[category];
 
   const interestRate = selectedTier.interestRate;
-  const downPaymentPercentage = selectedTier.downPaymentPercentage;
-  const termMultiplier = selectedTier.termMultiplier;
 
   const loanAmountAfterDownPayment = loanAmount - downPayment;
+
   const monthlyInterestRate = interestRate / 12;
-  const numberOfPayments = loanTerm * termMultiplier;
+
+  const numberOfPayments = loanTerm;
 
   const monthlyPayment =
-    (loanAmountAfterDownPayment * monthlyInterestRate) /
-    (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+      (loanAmountAfterDownPayment * monthlyInterestRate) /
+      (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
 
   const totalPayment = monthlyPayment * numberOfPayments + downPayment;
-
-  // Calculate the total interest paid
-  const totalInterest = totalPayment - loanAmountAfterDownPayment;
 
   // Calculate the net interest rate
   const netInterest = totalPayment-loanAmount;
 
   // Create a loan details object
   const loanDetails = {
-    category: category,
-    downPayment: downPayment,
-    loanTerm: loanTerm,
-    monthlyPayment: monthlyPayment.toFixed(2),
-    netInterest: netInterest.toFixed(2),
-    totalPayment: totalPayment.toFixed(2),
+      category: category,
+      downPayment: downPayment,
+      loanTerm: loanTerm,
+      monthlyPayment: monthlyPayment.toFixed(2),
+      netInterest: netInterest.toFixed(2),
+      totalPayment: totalPayment.toFixed(2),
   };
 
   // Save the new loan details to localStorage
@@ -65,7 +62,7 @@ function calculateLoan() {
   const loanResult = document.getElementById('loanResult');
   loanResult.innerHTML = `
     <p>Category: ${category}</p>
-    <p>Loan Ammount: ${loanAmount}</p>
+    <p>Loan Amount: Rs${loanAmount}</p>
     <p>Down Payment: Rs${downPayment}</p>
     <p>Loan Term: ${loanTerm} months</p>
     <p>Monthly Payment: Rs${monthlyPayment.toFixed(2)}</p>
@@ -73,16 +70,15 @@ function calculateLoan() {
     <br>
     <p>Total Payment: Rs${totalPayment.toFixed(2)}</p>
   `;
+}
   
-}
+  // Function to clear the existing loan data from localStorage
+  function clearLoanStorage() {
+    localStorage.removeItem('loans');
+  }
+  
 
-// Function to clear the existing loan data from localStorage
-function clearLoanStorage() {
-  localStorage.removeItem('loans');
-}
-
-
-'use strict';
+  'use strict';
 
 const overlay = document.querySelector("[data-overlay]");
 const navbar = document.querySelector("[data-navbar]");
@@ -90,15 +86,15 @@ const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
 const navToggleFunc = function () {
-navToggleBtn.classList.toggle("active");
-navbar.classList.toggle("active");
-overlay.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
 };
 
 navToggleBtn.addEventListener("click", navToggleFunc);
 overlay.addEventListener("click", navToggleFunc);
 
 for (let i = 0; i < navbarLinks.length; i++) {
-navbarLinks[i].addEventListener("click", navToggleFunc);
+  navbarLinks[i].addEventListener("click", navToggleFunc);
 }
 const header = document.querySelector("[data-header]");
